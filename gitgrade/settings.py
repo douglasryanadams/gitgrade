49f9c5e5aa939c80ab4a8f55d4654a000ba0ad98
Douglas Adams <douglasryanadams@gmail.com>
2022-01-15 18:44:18 -0800

    Remove secret

--- gitgrade/settings.py
+++ gitgrade/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u$zro#3q9htls7mt$e2)w33c*#103)!k2g9*19-dalu+j142qd"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
